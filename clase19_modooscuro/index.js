e9d972382342b2017f6a46fbd8d4d63c29094616
NathaVic <nathavic@Alf-BuscaFama.local>
2020-09-01 23:47:30 -0300

    clase 19 / modo oscuro

---
+++ clase19_modooscuro/index.js
@@ -0,0 +1,19 @@
+// modo-oscuro
+// Crear un documento html con un título, un párrafo y un bótón con un ícono. 
+// Centrar los elementos, cambiar los tamañas y tipografía del texto y tamaño del ícono. 
+// Hacer que el body ocupe toda la pantalla. Al clickear el botón, debe alternar entre modo oscuro y modo claro:
+
+// modo oscuro: body con color de fondo negro, texto e ícono con color blanco, ícono de sol
+// modo claro: body con color de fondo blanco, texto e ícono con color negro, ícono de luna
+
+const modoOscuro = document.querySelector('#cambio')
+
+modoOscuro.onclick = () => {
+    document.body.classList.toggle("oscuro")
+    modoOscuro.classList.toggle("ativo")
+
+    if (luna.contains("ativo") === "true") {
+        document.body.classList.add("oscuro")
+        modoOscuro.classList.add("ativo")
+    }
+}
